56d87b3298e2c755ddb8fbbfaac8bee679a95b54
Alexander Villamil <alxvilbol@gmail.com>
2018-09-09 18:16:01 -0500

    creating hello world example

--- intro_app/src/index.js
+++ intro_app/src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+var React = require('react');
+var ReactDOM = require('react-dom');
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+//<h1>Hello World</h1>
+// In react if we want to do the same thing, we first need to create a react
+// element of type h1 with content 'Hello World' which will eventually spit out
+// heading onto the webpage
+
+var App = React.createElement('h1', null, 'Hello World');
+
+// We need to somehow tell react to take our React element(App) and display it
+// on the webpage using DOM
+//ReactDOM.render([What], [Where])
+ReactDOM.render(
+  App,
+  document.getElementById('root')
+);
