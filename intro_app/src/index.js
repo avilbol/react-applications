90bf232ab6049195add7397070b9446e4dca4b02
Alexander Villamil <alxvilbol@gmail.com>
2018-09-09 21:26:01 -0500

    dynamic hello world with class component exp1

--- intro_app/src/index.js
+++ intro_app/src/index.js
@@ -1,38 +1,22 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var createClass = require('create-react-class');
 
-//<h1>Hello World</h1>
-// In react if we want to do the same thing, we first need to create a react
-// element of type h1 with content 'Hello World' which will eventually spit out
-// heading onto the webpage
+// var Component = function(props){
+//   return(
+//     <div style={{ color : props.color }}>
+//       <h1>{props.greeting}</h1>
+//     </div>
+//   );
+// };
 
-// React.createElement(type, attrs/props, child1, child2, child3, ...);
-// React.createElement(type, attrs/props, ...children);
-//var App = React.createElement('h1', null, 'Hello World');
-// var app = <h1>Hello World</h1>
+// npm i -S create-react-class
 
-var Component = function(props){
-  return(
-    <div style={{ color : props.color }}>
-      <h1>{props.greeting}</h1>
-    </div>
-  );
-};
+var Component = createClass({
 
-
-// We need to somehow tell react to take our React element(App) and display it
-// on the webpage using DOM
-//ReactDOM.render([What], [Where])
-
-// ReactDOM.render(
-//   React.createElement(Component),
-//   document.getElementById('root')
-// );
+});
 
 ReactDOM.render(
-  <div>
-    <Component greeting="Hello world" color="blue"/>
-    <Component greeting="Hey there" color="green"/>
-  </div>,
+  <Component greeting="Hello world" color="blue"/>,
   document.getElementById('root')
 );
